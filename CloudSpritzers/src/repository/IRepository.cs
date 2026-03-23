0f6dfcb8759d3d3b7ce06638cd7404d260b09b83
MiusR <ratiumihaic@gmail.com>
2026-03-23 12:57:10 +0200

    rebased IRepository

--- CloudSpritzers/src/repository/IRepository.cs
+++ CloudSpritzers/src/repository/IRepository.cs
@@ -1,7 +1,16 @@
-public interface IRepository<K, V> where V : class
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSpritzers.src.repository
 {
-    V GetById(K id);
-    K Add(V elem);
-    void DeleteById(K id);
-    void UpdateById(K id, V elem);
+    public interface IRepository<K, V> where V : class
+    {
+        V GetById(K id);
+        K Add(V elem);
+        void DeleteById(K id);
+        void UpdateById(K id, V elem);
+    }
 }
