8eef83aafc78a43efe6d47dfd78fd644d43e6c6e
MiusR <ratiumihaic@gmail.com>
2026-03-23 20:27:31 +0200

    created Message class

---
+++ CloudSpritzers/src/model/message/Message.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSpritzers.src.model.message
+{
+    // FIXME: Remove this once the actual classes are implemented
+    using IResponder = Object;
+    using Chat = Object;
+    public class Message : IMessage
+    {
+        private int id;
+        private IResponder sender;
+        private Chat chat;
+        private DateTimeOffset timestamp;
+        private string messageText;
+        private bool isRead;
+
+        public Message(Object sender, Object chat, string messageText, bool isRead)
+        {
+            this.sender = sender;
+            this.chat = chat;
+            this.messageText = messageText;
+            this.isRead = isRead;
+            this.timestamp = DateTimeOffset.UtcNow;
+        }
+
+        void MarkAsRead()
+        {
+            this.isRead = true;
+        }
+
+        bool isMessageRead()
+        {
+            return isRead;
+        }
+
+        Chat getChat()
+        {
+            return this.chat;
+        }
+
+        // Interface functionality
+
+        public string GetMessage()
+        {
+            return this.messageText;
+        }
+
+        public IResponder GetSender()
+        {
+            return sender;
+        }
+
+        IEnumerable<IMessage> GetNextOptions()
+        {
+            return new List<IMessage>();
+        }
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        DateTimeOffset getTimeStamp()
+        {
+            return getTimeStamp();
+        }
+    }
+}
