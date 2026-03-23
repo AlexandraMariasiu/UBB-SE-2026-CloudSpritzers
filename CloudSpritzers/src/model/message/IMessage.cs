3173202a6fe2d7bb3ce20f0d4e1a006dec142552
MiusR <ratiumihaic@gmail.com>
2026-03-23 20:27:13 +0200

    created IMessage interface

---
+++ CloudSpritzers/src/model/message/IMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSpritzers.src.model.message
+{
+    internal interface IMessage
+    {
+        string GetMessage();
+        IEnumerable<IMessage> GetNextOptions();
+
+        //FIXME: this should return an IResponder but we have to wait for implementation 
+        Object GetSender();
+
+        int GetId();
+
+        DateTimeOffset getTimeStamp();
+
+    }
+}
