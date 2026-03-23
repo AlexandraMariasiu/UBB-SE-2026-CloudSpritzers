cb5e3cd281b8b06cd46daa30496e821c50064908
Denisa847 <denisa.ioana.sas@stud.ubbcluj.ro>
2026-03-23 15:15:43 +0200

    faq

---
+++ CloudSpritzers/src/model/FAQCategoryEnum.cs
@@ -0,0 +1,12 @@
+namespace CloudSpritzers.src.model
+{
+    public enum FAQCategoryEnum
+    {
+        CheckIn,
+        Parking,
+        Baggage,
+        Tickets,
+        Facilities,
+        All
+    }
+}
