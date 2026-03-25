747f5c8b81416ee8dd6c65e16cb99b62501c4ff6
Alex <alexia.stoicescu@stud.ubbcluj.ro>
2026-03-25 20:01:34 +0200

    UrgencyLevel is now given by Category and can be changed later

--- CloudSpritzers/src/model/ticket/Ticket.cs
+++ CloudSpritzers/src/model/ticket/Ticket.cs
@@ -6,28 +6,33 @@ namespace CloudSpritzers.src.model.ticket
     {
         public int TicketId { get; }
         public User User { get; }
-        public UrgencyLevelEnum UrgencyLevel { get; }
+        public UrgencyLevelEnum UrgencyLevel { get; private set; }
         public StatusEnum Status { get; private set; }
         public TicketCategory Category { get; }
         public TicketSubcategory Subcategory { get; }
         public string Subject { get; }
         public string Description { get; }
         public DateTime CreatedAt { get; }
-        public Ticket(int ticketId, User user, UrgencyLevelEnum urgencyLevel, StatusEnum status, TicketCategory category, TicketSubcategory subcategory, string subject, string description, DateTime createdAt)
-        {
+        public Ticket(int ticketId, User user, StatusEnum status, TicketCategory category, TicketSubcategory subcategory, string subject, string description, DateTime createdAt)
+        { 
             TicketId = ticketId;
             User = user;
-            UrgencyLevel = urgencyLevel;
+            UrgencyLevel = category.UrgencyLevel;
             Status = status;
             Category = category;
             Subcategory = subcategory;
             Subject = subject;
             Description = description;
             CreatedAt = createdAt;
-            }
+        }
         public void UpdateStatus(StatusEnum newStatus)
         {
             this.Status = newStatus;
+        }
+
+        public void ChangeUrgencyLevel(UrgencyLevelEnum urgencyLevel)
+        {
+            this.UrgencyLevel = urgencyLevel;
         }
     }
  }
