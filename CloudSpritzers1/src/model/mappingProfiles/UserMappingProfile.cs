100365f0a3eca6f930b533828945f1e20aa585d6
diana-reut <dianaradauti2013@gmail.com>
2026-03-28 13:49:17 +0200

    created UserMappingProfile

---
+++ CloudSpritzers1/src/model/mappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CloudSpritzers.src.dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSpritzers.src.model.mappingProfiles
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, UserDTO>()
+            
+            .ForMember(dest => dest.UserId,
+                       opt => opt.MapFrom(src => src.UserId.ToString()))
+            
+            .ForMember(dest => dest.Name,
+                       opt => opt.MapFrom(src => src.GetName()))
+            
+            .ForMember(dest => dest.Email,
+                       opt => opt.MapFrom(src => src.GetEmail()))
+
+            .ConstructUsing(src => new UserDTO(
+                src.UserId.ToString(),
+                src.GetName(),
+                src.GetEmail()
+            ));
+        }
+    }
+}
