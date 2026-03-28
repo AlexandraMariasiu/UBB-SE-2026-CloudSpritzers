1885ae428faca57e9d1769019b66e7b89ebedd41
Alexandra <alexandra_stanca08@yahoo.com>
2026-03-29 00:06:34 +0200

    Added the mapping profile

---
+++ CloudSpritzers1/src/model/mappingProfiles/ReviewMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CloudSpritzers1.src.model.review;
+using CloudSpritzers1.src.DTO;
+
+namespace CloudSpritzers1.src.model.mappingProfiles
+{
+    public class RevieMappingProfile : Profile
+    {
+        public RevieMappingProfile()
+        {
+            System.Diagnostics.Debug.WriteLine("ReviewMappingProfile Loaded!");
+            CreateMap<Review, ReviewDTO>();
+        }
+    }
+}
