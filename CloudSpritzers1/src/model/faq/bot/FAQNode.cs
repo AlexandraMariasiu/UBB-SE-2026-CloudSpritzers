baab5dbd8aa7575c1a6f497b9dff03cb9a15e2c1
MiusR <ratiumihaic@gmail.com>
2026-03-28 17:45:52 +0200

    Rebranded ButtonDialogOption

---
+++ CloudSpritzers1/src/model/faq/bot/FAQNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudSpritzers.src.model.message;
+using CloudSpritzers1.src.service.bot;
+
+namespace CloudSpritzers1.src.model.faq.bot
+{ 
+    public record FAQNode(
+        int FaqNodeId,
+        string QuestionText,
+        List<FAQOption> Options,
+        bool IsFinalAnswer
+    );
+}
---
+++ CloudSpritzers1/src/service/bot/BotEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudSpritzers.src.model.message;
+using CloudSpritzers1.src.model.faq.bot;
+
+namespace CloudSpritzers1.src.service.bot
+{
+    public class BotEngine : IResponder
+    {
+
+        private Dictionary<string, IMessage> currentSelection;
+
+        private IMessage currentDiscussionNode;
+
+        public IMessage Respond(IMessage message)
+        {
+            string text = message.GetMessage();
+
+            if(!currentSelection.ContainsKey(text))
+            {
+                // TODO take this from repository, map to db the chat options
+                return new FAQNode.Builder(-1, "Sorry, I am experiencing some technical difficulties. Could you start again?", "")
+                    .AddOption(
+                        new FAQNode.Builder(-1, "Yes", "")
+                    )
+            }
+
+            return null;
+        }
+        
+        public string GetEmail()
+        {
+            return "customer-support@cloudspritzers.com";
+        }
+
+        public string GetName()
+        {
+            return "Carlos";
+        }
+    }
+}
