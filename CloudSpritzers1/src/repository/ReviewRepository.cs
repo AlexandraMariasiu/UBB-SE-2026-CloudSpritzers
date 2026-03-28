e402f7f7d826890759260fa9af033d66bea60de1
Alexandra <alexandra_stanca08@yahoo.com>
2026-03-28 21:58:21 +0200

    Added throw exception to ReviewRepository

--- CloudSpritzers1/src/repository/ReviewRepository.cs
+++ CloudSpritzers1/src/repository/ReviewRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using CloudSpritzers1.src.model.review;
@@ -6,44 +7,50 @@ namespace CloudSpritzers1.src.repository
 {
     public class ReviewRepository : IRepository<int, Review>
     {
-        private Dictionary<int, Review> reviews = new Dictionary<int, Review>();
+        private Dictionary<int, Review> _reviews = new Dictionary<int, Review>();
 
         public Review GetById(int id)
         {
-            return reviews.ContainsKey(id) ? reviews[id] : null;
+            if (!_reviews.ContainsKey(id))
+                throw new KeyNotFoundException($"Review with id {id} was not found.");
+            return _reviews[id];
         }
 
         public int Add(Review elem)
         {
-            if(reviews.ContainsKey(elem.GetId()))
-            {
-                return -1; // or throw an exception
-            }
-            reviews[elem.GetId()] = elem;
+            if (_reviews.ContainsKey(elem.GetId()))
+                throw new InvalidOperationException($"Review with id {elem.GetId()} already exists.");
+            if (elem == null)
+                throw new ArgumentNullException(nameof(elem), "Review cannot be null.");
+            _reviews[elem.GetId()] = elem;
             return elem.GetId();
         }
 
         public void UpdateById(int id, Review elem)
         {
-            if (reviews.ContainsKey(id))
-            {
-                reviews[id] = elem;
-            }
+            if (!_reviews.ContainsKey(id))
+                throw new KeyNotFoundException($"Review with id {id} was not found.");
+
+            if (elem == null)
+                throw new ArgumentNullException(nameof(elem), "Review cannot be null.");
+
+            _reviews[id] = elem;
+
         }
 
         public void DeleteById(int id)
         {
-            if (reviews.ContainsKey(id))
-            {
-                reviews.Remove(id);
-            }
-        }
+            if (!_reviews.ContainsKey(id))
+                throw new KeyNotFoundException($"Review with id {id} was not found.");
 
+            _reviews.Remove(id);
+
+        }
 
         // same as FindAll() from UML
         public IEnumerable<Review> GetAll()
         {
-            return reviews.Values.ToList();
+            return _reviews.Values.ToList();
         }
 
     }
