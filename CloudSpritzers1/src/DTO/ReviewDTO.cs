edc493ad9267a4c5fee4ee4e74a3a95f7b61ba35
Alexandra <alexandra_stanca08@yahoo.com>
2026-03-29 15:12:05 +0300

    changed the ReviewDTO into record

--- CloudSpritzers1/src/DTO/ReviewDTO.cs
+++ CloudSpritzers1/src/DTO/ReviewDTO.cs
@@ -6,29 +6,16 @@ using System.Threading.Tasks;
 
 namespace CloudSpritzers1.src.DTO
 {
-    public class ReviewDTO
+    namespace CloudSpritzers1.src.DTO
     {
-        public int id { get; set; }
-        public int userId { get; set; }   // only the id, for security of user
-        public string message { get; set; }
-        public int dutyFreeRating { get; set; }
-        public int flightExperienceRating { get; set; }
-        public int staffFriendlinessRating { get; set; }
-        public int cleanlinessRating { get; set; }
-
-        public ReviewDTO() { }
-
-        public ReviewDTO(int id, int userId, string message, int dutyFreeRating, int flightExperienceRating, int staffFriendlinessRating, int cleanlinessRating)
-        {
-            this.id = id;
-            this.userId = userId;
-            this.message = message;
-            this.dutyFreeRating = dutyFreeRating;
-            this.flightExperienceRating = flightExperienceRating;
-            this.staffFriendlinessRating = staffFriendlinessRating;
-            this.cleanlinessRating = cleanlinessRating;
-        }
+        public record ReviewDTO(
+            int id,
+            int userId,
+            string message,
+            int dutyFreeRating,
+            int flightExperienceRating,
+            int staffFriendlinessRating,
+            int cleanlinessRating
+        );
     }
-
-
 }
