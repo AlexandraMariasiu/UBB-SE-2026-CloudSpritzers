6a8f0a878adab90f77bbaffe02cf60570cae10ae
Alexandra <alexandra_stanca08@yahoo.com>
2026-03-29 13:57:06 +0300

    Changed the ReviewMappingProfile

--- CloudSpritzers1/src/model/mappingProfiles/ReviewMappingProfile.cs
+++ CloudSpritzers1/src/model/mappingProfiles/ReviewMappingProfile.cs
@@ -9,7 +9,37 @@ namespace CloudSpritzers1.src.model.mappingProfiles
         public RevieMappingProfile()
         {
             System.Diagnostics.Debug.WriteLine("ReviewMappingProfile Loaded!");
-            CreateMap<Review, ReviewDTO>();
+            CreateMap<Review, ReviewDTO>()
+            .ForMember(dest => dest.id,
+                        opt => opt.MapFrom(src => src.GetId()))
+
+            .ForMember(dest => dest.userId,
+                        opt => opt.MapFrom(src => src.GetUser().UserId))
+
+            .ForMember(dest => dest.message,
+                        opt => opt.MapFrom(src => src.GetMessage()))
+
+            .ForMember(dest => dest.dutyFreeRating,
+                        opt => opt.MapFrom(src => src.GetDutyFreeRating()))
+
+            .ForMember(dest => dest.flightExperienceRating,
+                        opt => opt.MapFrom(src => src.GetFlightExperienceRating()))
+
+            .ForMember(dest => dest.staffFriendlinessRating,
+                        opt => opt.MapFrom(src => src.GetStaffFriendlinessRating()))
+
+            .ForMember(dest => dest.cleanlinessRating,
+                        opt => opt.MapFrom(src => src.GetCleanlinessRating()))
+
+            .ConstructUsing(src => new ReviewDTO(
+                src.GetId(),
+                src.GetUser().UserId,
+                src.GetMessage(),
+                src.GetDutyFreeRating(),
+                src.GetFlightExperienceRating(),
+                src.GetStaffFriendlinessRating(),
+                src.GetCleanlinessRating()
+            ));
         }
     }
 }
