cb8d6def2281e46175fb4ca876075ba515619eb8
Mekk13 <mariasuciu313@gmail.com>
2026-03-31 12:53:07 +0300

    Created Top Bar

---
+++ CloudSpritzers1/src/view/general/UpperBar.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace CloudSpritzers1.src.view.general
+{
+    public sealed partial class UpperBar : UserControl
+    {
+        public UpperBar()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
