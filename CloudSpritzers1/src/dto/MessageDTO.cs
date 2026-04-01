14bee0bdcfa4f85550eeb0dde622f0cffc30521f
Simon Laszlo <xbeanmanx@protonmail.com>
2026-04-01 07:57:01 +0300

    Rewrote MessageService, corrected MessageDBRepository, implemented MessageViewModel and corrected MessageView to have proper data binding to it's fields. !-- CHECK TODO-s AND CORRECTED DTO NAMESPACE INCONSISTENCY--!

--- CloudSpritzers1/src/dto/MessageDTO.cs
+++ CloudSpritzers1/src/dto/MessageDTO.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections.Generic;
 using CloudSpritzers1.src.model.faq.bot;
 
-namespace CloudSpritzers1.src.DTO
+namespace CloudSpritzers1.src.dto
 {
     public record MessageDTO(
         int MessageId,
         int ChatId,
-        int SenderId,       // numeric FK matching the DB column sender_id
+        int SenderId,
         string MessageText,
         DateTimeOffset Timestamp,
         IEnumerable<FAQOption> FaqOptions
