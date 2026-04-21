c26eb46b703458075d9f67451ce7669700dd3520
AlexandraMariasiu <alemariasiu@yahoo.com>
2026-04-21 21:51:02 +0300

    Fix namespace casing in IFAQRepository

--- CloudSpritzers1/src/repository/interface/IFAQRepository.cs
+++ CloudSpritzers1/src/repository/interface/IFAQRepository.cs
@@ -5,13 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using CloudSpritzers1.Src.Model.Faq;
 
-<<<<<<< HEAD
-namespace CloudSpritzers1.Src.Repository.Interfaces
-=======
-namespace CloudSpritzers1.src.repository.interfaces;
+namespace CloudSpritzers1.Src.Repository.Interfaces;
 
 public interface IFAQRepository : IRepository<int, FAQEntry>
->>>>>>> f13abe4dcf77fd075160dbe0705442508e85ce1c
 {
     List<FAQEntry> GetByCategory(FAQCategoryEnum category);
     void IncrementViewCount(int id);
